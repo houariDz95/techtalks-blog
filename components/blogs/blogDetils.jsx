69b7870b87017b252f56d2d0662f49b4ba0cf903
eddDevDz95 <houedd608@gmail.com>
2024-02-12 23:09:31 +0100

    new commit

--- components/blogs/blogDetils.jsx
+++ components/blogs/blogDetils.jsx
@@ -5,7 +5,7 @@ import { calculateReadingTime } from "@/lib/utils";
 //import ViewCounter from "./ViewCounter";
 
 const BlogDetails = ({ blog }) => {
-    const readingTime = calculateReadingTime(blog.desc)
+   
   return (
     <div className="px-2  md:px-10 bg-accentSoft dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
       <time className="m-3">
@@ -15,7 +15,7 @@ const BlogDetails = ({ blog }) => {
         {/*<ViewCounter slug={blogSlug} />*/}
         <div>{blog.views} views</div>
       </span>
-      <div className="m-3">{readingTime} min reads</div>
+      <div className="m-3">5 min reads</div>
       <Link href={`/blog?s=${blog.category}`} className="m-3">
         #{blog.category}
       </Link>
